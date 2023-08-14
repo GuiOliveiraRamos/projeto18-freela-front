b94d0ec8eb3f9ad980757358dd4dd040805c8e7a
GuiOliveiraRamos <guioliveiraramos30@gmail.com>
2023-08-14 10:57:55 -0300

    header responsivo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,53 @@
-import { styled } from "styled-components";
+import React, { useState } from "react";
+import styled from "styled-components";
 import {
   AiOutlineFacebook,
   AiOutlineTwitter,
   AiOutlineInstagram,
 } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 export default function Header() {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Container>
+      <MobileMenuButton onClick={toggleMenu}>
+        <FaBars />
+      </MobileMenuButton>
       <Title>
         <h1>
           Agência
           <br /> MiauMiau
         </h1>
       </Title>
-      <Content>
-        <p onClick={() => navigate("/homepage")}>Home</p>
-        <p onClick={() => navigate("/miaudelos")}>Miaudelos</p>
-        <p onClick={() => navigate("/my-miaudelos")}>Meu perfil</p>
-        <p>Sobre nós</p>
-        <p>Contato</p>
-      </Content>
+
+      <ContentDesktop>
+        <MenuItem onClick={() => navigate("/homepage")}>Home</MenuItem>
+        <MenuItem onClick={() => navigate("/miaudelos")}>Miaudelos</MenuItem>
+        <MenuItem onClick={() => navigate("/my-miaudelos")}>
+          Meu perfil
+        </MenuItem>
+        <MenuItem>Sobre nós</MenuItem>
+        <MenuItem>Contato</MenuItem>
+      </ContentDesktop>
+      {isMenuOpen && (
+        <MobileMenu>
+          <MenuItem onClick={() => navigate("/homepage")}>Home</MenuItem>
+          <MenuItem onClick={() => navigate("/miaudelos")}>Miaudelos</MenuItem>
+          <MenuItem onClick={() => navigate("/my-miaudelos")}>
+            Meu perfil
+          </MenuItem>
+          <MenuItem>Sobre nós</MenuItem>
+          <MenuItem>Contato</MenuItem>
+        </MobileMenu>
+      )}
       <SocialMedias>
         <AiOutlineFacebook />
         <AiOutlineTwitter />
@@ -34,7 +59,7 @@ export default function Header() {
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   background-color: #fff2eb;
   position: fixed;
@@ -43,6 +68,9 @@ const Container = styled.div`
   top: 0;
   left: 0;
   z-index: 10;
+  @media (max-width: 768px) {
+    justify-content: space-between;
+  }
 `;
 
 const Title = styled.div`
@@ -54,16 +82,57 @@ const Title = styled.div`
     font-family: "Poppins", sans-serif;
     font-weight: 100;
   }
+  @media (max-width: 768px) {
+    padding-right: 60px;
+  }
 `;
 
-const Content = styled.div`
+const MobileMenuButton = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    font-size: 30px;
+    cursor: pointer;
+    padding-left: 30px;
+  }
+`;
+
+const ContentDesktop = styled.div`
   width: 600px;
   display: flex;
   justify-content: space-between;
-  p {
-    font-size: 20px;
-    font-family: "Poppins", sans-serif;
-    cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const MobileMenu = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    background-color: #fff2eb;
+    position: fixed;
+    top: 120px;
+    left: 0;
+    width: 100%;
+    padding: 20px;
+    z-index: 10;
+  }
+`;
+
+const MenuItem = styled.p`
+  font-size: 20px;
+  font-family: "Poppins", sans-serif;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    margin-bottom: 10px;
   }
 `;
 
@@ -72,4 +141,8 @@ const SocialMedias = styled.div`
   display: flex;
   justify-content: space-around;
   font-size: 30px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
